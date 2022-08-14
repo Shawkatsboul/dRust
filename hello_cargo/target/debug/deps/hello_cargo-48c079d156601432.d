4243c9b77f00922ce93908b58bb71d911006b52f
Shawkatsboul <Shawkatsboul@me.com>
2022-08-14 16:15:47 +0100

    cargo

---
+++ hello_cargo/target/debug/deps/hello_cargo-48c079d156601432.d
@@ -0,0 +1,5 @@
+/home/shawkat/dRust/hello_cargo/target/debug/deps/hello_cargo-48c079d156601432: src/main.rs
+
+/home/shawkat/dRust/hello_cargo/target/debug/deps/hello_cargo-48c079d156601432.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/shawkat/dRust/hello_cargo/target/debug/hello_cargo: /home/shawkat/dRust/hello_cargo/src/main.rs
