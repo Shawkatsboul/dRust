a5685cd789ef145577e9face9f44222c227f7fa4
Shawkatsboul <Shawkatsboul@me.com>
2022-08-16 13:33:48 +0100

    guessing_game

---
+++ guessing_game/target/debug/deps/guessing_game-354c3bbb0fd628dd.d
@@ -0,0 +1,5 @@
+/home/shawkat/dRust/guessing_game/target/debug/deps/guessing_game-354c3bbb0fd628dd: src/main.rs
+
+/home/shawkat/dRust/guessing_game/target/debug/deps/guessing_game-354c3bbb0fd628dd.d: src/main.rs
+
+src/main.rs:
---
+++ guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+/home/shawkat/dRust/guessing_game/target/debug/guessing_game: /home/shawkat/dRust/guessing_game/src/main.rs
